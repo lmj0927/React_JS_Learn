0feb12120290ee86c9247a8e3b5a76b9cca5376d
lmj0927 <32671179+lmj0927@users.noreply.github.com>
2022-04-12 22:32:15 +0900

    React_useEffeact

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("created");
+    return () => console.log("destroyed");
+  }, []);
+  return <h1>Hello</h1>;
+}
 
 export default function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Continue"} />
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
